57501fc97bcb471542766761e1c2fdaea96328a2
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2024-11-09 22:57:26 +0000

    [Proj02_books] - Added put and delete

--- fastapi_udemy_course/proj02_books/app.py
+++ fastapi_udemy_course/proj02_books/app.py
@@ -2,13 +2,18 @@
 """
 
 # Builtin imports
-from typing import Optional
+from typing import Optional, TYPE_CHECKING, Union
 
 # Project specific imports
 from fastapi import FastAPI, status, HTTPException, Body
+from fastapi.encoders import jsonable_encoder
+
 
 # Local imports
-from .model import Book
+from .model import Book, UpdateBook
+
+if TYPE_CHECKING:
+    from uuid import UUID
 
 #-----------------------------------------------------------------------------#
 # APP
@@ -20,12 +25,20 @@ app = FastAPI()
 # Database
 #-----------------------------------------------------------------------------#
 
-BOOKS = [Book(id=1, title='The Alchemist', author='Paulo Coelho', description='Great Book', rating=4),
-         Book(id=2, title='My Name is Red', author='Orhan Pamuk', description='One of the best books', rating=4),
-         Book(id=3, title='Jurassic Park', author='Michael Crichton', description='Amazing Book', rating=3),
-         Book(id=4, title='1984', author='George Orwell', description='Best SciFi', rating=3),
-         Book(id=5, title='Animal Farm', author='George Orwell', description='Great Book', rating=4),
-         Book(id=6, title='The Pilgrimage', author='Paulo Coelho', description='Great Book', rating=2)]
+BOOKS = [Book(title='The Alchemist', author='Paulo Coelho', description='Great Book', rating=4),
+         Book(title='My Name is Red', author='Orhan Pamuk', description='One of the best books', rating=4),
+         Book(title='Jurassic Park', author='Michael Crichton', description='Amazing Book', rating=3),
+         Book(title='1984', author='George Orwell', description='Best SciFi', rating=3),
+         Book(title='Animal Farm', author='George Orwell', description='Great Book', rating=4),
+         Book(title='The Pilgrimage', author='Paulo Coelho', description='Great Book', rating=2)]
+
+#-----------------------------------------------------------------------------#
+# Utils
+#-----------------------------------------------------------------------------#
+def encode_input(data) -> dict[str, Optional[Union[str, int]]]:
+    data = jsonable_encoder(data)
+    data = {k: v for k, v in data.items() if v is not None}
+    return data
 
 #-----------------------------------------------------------------------------#
 # Routes
@@ -40,5 +53,29 @@ async def create_book(book: Book) -> Book:
     return BOOKS[-1]
 
 @app.get("/books", tags=['Books'])
-async def get_all_books() -> list[Book]:
-    return BOOKS
+async def get_all_books(rating: Optional[int] = None) -> list[Book]:
+    if rating == None:
+        return BOOKS
+    return [book for book in BOOKS if book.rating == rating]
+
+@app.get("/books/{id}", tags=['Books'])
+async def get_book(id: str) -> Optional[Book]:
+    for book in BOOKS:
+        if str(book.id) == id:
+            return book
+
+@app.put("/books/{id}", tags=['Books'], response_model=UpdateBook)
+async def update_book(id: str, data_to_update:UpdateBook)-> Book:
+    for index, book in enumerate(BOOKS):
+        if str(book.id) == id:
+            # cleanup dict
+            data = encode_input( data_to_update.model_dump() )
+            BOOKS[index] = book.model_copy(update=data)
+            return BOOKS[index]                
+
+@app.delete("/books/{id}", tags=['Books'])
+async def delete_book(id: str):
+    for index, book in enumerate(BOOKS):
+        if str(book.id) == id:
+            del BOOKS[index]
+            return
--- fastapi_udemy_course/proj02_books/model.py
+++ fastapi_udemy_course/proj02_books/model.py
@@ -1,12 +1,23 @@
 """
 """
 
+# Builtin imports
+import uuid
+from typing import Optional
+
 # Project specific imports
 from pydantic import BaseModel, Field
 
 class Book(BaseModel):
-    id: int = Field(gt=0)
+    id: uuid.UUID = Field(init=False, default_factory=uuid.uuid4)
     title: str
     author: str
     description: str
     rating: int = Field(gt=0, lt=6)
+
+class UpdateBook(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    description: Optional[str] = None
+    rating: Optional[int] = None
+

5e8bf529ba247c1bd7b1e4956f945bc9db26e97e
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2024-11-10 21:33:31 +0000

    [Proj03_todo] - Added database.py

---
+++ fastapi_udemy_course/proj03_todo/database.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+#-----------------------------------------------------------------------------#
+# Setup
+#-----------------------------------------------------------------------------#
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+ENGINE = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SESSIONLOCAL = sessionmaker(autoflush=False, bind=ENGINE)
+BASE = declarative_base()

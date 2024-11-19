119c6d72bb5050650ec5cda99b97a93124614b9b
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2024-11-19 14:43:22 +0000

    Added a todoitem to the db as part of initial setup

--- tests/test_proj03_todo/conftest.py
+++ tests/test_proj03_todo/conftest.py
@@ -2,6 +2,7 @@
 Define fixtures
 """
 
+import os
 import pytest
 
 # Project specific imports
@@ -10,11 +11,35 @@ from fastapi.testclient import TestClient
 # Local imports
 from fastapi_udemy_course.proj03_todo.app import app
 from fastapi_udemy_course.proj03_todo.routers.auth import get_current_user
+from fastapi_udemy_course.proj03_todo.database.model import Todos
+from fastapi_udemy_course.proj03_todo import db_init
 
 def current_user():
     return {'email_address': 'praveen@gmail.com', 'id': 1, 'role': 'admin'}
 
 @pytest.fixture(scope="session")
-def client():
+def test_db():
+    # Clear the db file if it exists
+    db_url = os.environ['DB_SQLITE']
+    if os.path.exists(db_url):
+        os.remove(db_url)
+
+    # Get the db instance
+    db = next(db_init.get_db())
+
+    # Populate
+    todoitem_1 = Todos(title="Test", description="Test item 1", priority=5, complete=True, owner_id = 1)
+    db.add(todoitem_1)
+    db.commit()
+
+    # Yield
+    yield db
+
+    # Cleanup
+    db.query(Todos).filter(Todos.id == 1).delete()
+    db.commit()
+
+@pytest.fixture(scope="session")
+def client(test_db):
     app.dependency_overrides[get_current_user] = current_user
     return TestClient(app)
--- tests/test_proj03_todo/test_todos.py
+++ tests/test_proj03_todo/test_todos.py
@@ -7,3 +7,5 @@ from fastapi import status
 def test_get_all_todos(client):
     response = client.get("/todos")
     assert response.status_code == status.HTTP_200_OK
+    items = response.json()
+    assert items[0]['id'] == 1

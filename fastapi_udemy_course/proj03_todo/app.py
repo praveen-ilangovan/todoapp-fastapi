0b9a0c989a24e609f3695e2fa0cb2b1d06bf3bb4
Praveen Ilangovan <praveen.ilangovan@gmail.com>
2024-11-20 10:28:54 +0000

    Added jinja2

--- fastapi_udemy_course/proj03_todo/app.py
+++ fastapi_udemy_course/proj03_todo/app.py
@@ -3,12 +3,19 @@
 """
 
 # Project specific imports
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # Local imports
 from .database.database import ENGINE
 from .database import model
 from .routers import admin, auth, todos, user
+
+#-----------------------------------------------------------------------------#
+# Templates
+#-----------------------------------------------------------------------------#
+TEMPLATES = Jinja2Templates(directory="fastapi_udemy_course/proj03_todo/templates")
 
 #-----------------------------------------------------------------------------#
 # Database
@@ -24,14 +31,15 @@ app.include_router(todos.router, tags=["Todos"], prefix="/todos")
 app.include_router(admin.router, tags=["Admin"], prefix="/admin")
 app.include_router(user.router, tags=["Me"], prefix="/me")
 
+app.mount("/static", StaticFiles(directory="fastapi_udemy_course/proj03_todo/static"), name="static")
 
 
 #-----------------------------------------------------------------------------#
 # Routes
 #-----------------------------------------------------------------------------#
-# @app.get("/", tags=['Root'])
-# async def index() -> dict[str, str]:
-#     return {'message': 'Welcome ToDos App powered by sqlite'}
+@app.get("/", tags=['Root'])
+async def index(request: Request):
+    return TEMPLATES.TemplateResponse("home.html", {'request': request})
 
 @app.get("/health", tags=['Root'])
 async def health_check() -> dict[str, str]:
